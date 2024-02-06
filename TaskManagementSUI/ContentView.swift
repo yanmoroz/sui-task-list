af3506232f4914395fc9ff23fbeb079f9ddb4694
Yan Moroz <mail@yanmoroz.ru>
2024-02-06 14:41:40 +0300

    Initial Commit

---
+++ TaskManagementSUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskManagementSUI
+//
+//  Created by Yan Moroz on 06.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskManagementSUI/TaskManagementSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementSUIApp.swift
+//  TaskManagementSUI
+//
+//  Created by Yan Moroz on 06.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
